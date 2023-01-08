b4bd361560650cf2c9a5c4d4285cb02b9e4edc09
Siooraen <d1878279298@outlook.com>
2023-01-08 13:32:42 +0800

    initialization

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("io.izzel.taboolib") version "1.51"
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+}
+
+taboolib {
+    install("common")
+    install("common-5")
+    install("module-chat")
+    install("module-nms")
+    install("module-nms-util")
+    install("module-configuration")
+    install("platform-bukkit")
+    install("expansion-command-helper")
+    classifier = null
+    version = "6.0.10-39"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("ink.ptms:nms-all:1.0.0")
+    compileOnly("ink.ptms.core:v11902:11902-minimize:mapped")
+    compileOnly("ink.ptms.core:v11902:11902-minimize:universal")
+    compileOnly(kotlin("stdlib"))
+    compileOnly(fileTree("libs"))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjvm-default=all")
+    }
+}
---
+++ src/main/kotlin/me/siooraen/dropmanager/DropManager.kt
@@ -0,0 +1,10 @@
+package me.siooraen.dropmanager
+
+import taboolib.common.platform.Plugin
+
+/**
+ * @author Siooraen
+ * @since 2023/1/8 13:27
+ */
+object DropManager : Plugin() {
+}
